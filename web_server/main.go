edfd163e6be40e5a29827a3ab7ca539f21557086
Michael lemito <delovoy.lemit@gmail.com>
2025-06-15 00:10:00 +0300

    вынес в отдельный файлик + красота

--- web_server/main.go
+++ web_server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"embed"
 	"errors"
 	"fmt"
 	"html/template"
@@ -14,31 +15,21 @@ import (
 	"github.com/lemito/namingify/naming_client"
 )
 
-var formTemplate = `
-<!DOCTYPE html>
-<html>
-<head>
-	<title>UwU</title>
-</head>
-<body>
-	<h1>Meow</h1>
-	<form method="POST" action="/convert">
-		<label for="option">Method:</label>
-		<select name="option" id="option">
-			<option value="0">0</option>
-			<option value="1">1</option>
-			<option value="2">2</option>
-			<option value="3">3</option>
-		</select>
-		<br><br>
-		<label for="message">Lex:</label>
-		<input type="text" id="message" name="message" required>
-		<br><br>
-		<input type="submit" value="Send">
-	</form>
-</body>
-</html>
-`
+//go:embed templates
+var templateFS embed.FS
+
+var formTemplate *template.Template
+
+func init() {
+	tpl, err := template.ParseFS(templateFS, "templates/*.html")
+	if err != nil {
+		log.Fatalf("Failed to parse templates: %v", err)
+	}
+	formTemplate = tpl.Lookup("index.html")
+	if formTemplate == nil {
+		log.Fatal("Form template not found")
+	}
+}
 
 var fake_db = map[string][]byte{
 	"cat":   []byte("meow"),
@@ -57,55 +48,54 @@ var Group = groupcache.NewGroup("sounds", 64<<20, groupcache.GetterFunc(func(ctx
 	return nil
 }))
 
-func main() {
-
-	http.HandleFunc("/convert", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			tmpl, err := template.New("form").Parse(formTemplate)
-			if err != nil {
-				http.Error(w, "error", http.StatusInternalServerError)
-				return
-			}
-
-			tmpl.Execute(w, nil)
-		case http.MethodPost:
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, "Failed to parse form", http.StatusBadRequest)
-				return
-			}
-
-			message := r.FormValue("message")
-			opt_string := r.FormValue("option")
-
-			opt, err := strconv.Atoi(opt_string)
-			if err != nil {
-				log.Fatalf("Err %v", err)
-				http.Error(w, "error", http.StatusInternalServerError)
-				return
-			}
-
-			log.Printf("Received:\n Option %d \n Message: %s", opt, message)
-
-			msg, err := naming_client.Convert(message, opt)
-			if err != nil {
-				log.Fatalf("Err %v", err)
-				http.Error(w, "error", http.StatusInternalServerError)
-				return
-			}
-
-			_, err = fmt.Fprint(w, msg)
-			if err != nil {
-				log.Fatalf("Err %v", err)
-				http.Error(w, "error", http.StatusInternalServerError)
-				return
-			}
-		default:
-			http.Error(w, "No such method", http.StatusMethodNotAllowed)
+func convertHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		if err := formTemplate.Execute(w, nil); err != nil {
+			http.Error(w, "Template error", http.StatusInternalServerError)
+			log.Printf("Template error: %v", err)
+		}
+	case http.MethodPost:
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Failed to parse form", http.StatusBadRequest)
+			return
 		}
 
-	})
+		message := r.FormValue("message")
+		opt_string := r.FormValue("option")
+
+		opt, err := strconv.Atoi(opt_string)
+		if err != nil {
+			log.Fatalf("Err %v", err)
+			http.Error(w, "error", http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("Received:\n Option %d \n Message: %s", opt, message)
+
+		msg, err := naming_client.Convert(message, opt)
+		if err != nil {
+			log.Fatalf("Err %v", err)
+			http.Error(w, "error", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = fmt.Fprint(w, msg)
+		if err != nil {
+			log.Fatalf("Err %v", err)
+			http.Error(w, "error", http.StatusInternalServerError)
+			return
+		}
+	default:
+		http.Error(w, "No such method", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func main() {
+
+	http.HandleFunc("/convert", convertHandler)
 
 	http.HandleFunc("/sound", func(w http.ResponseWriter, r *http.Request) {
 		animal := r.URL.Query().Get("animal")
